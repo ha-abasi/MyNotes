315333c8096f2cea0849687ff6c972d3f6ef626b
ha-abasi <139422110+altyn-it@users.noreply.github.com>
2024-12-16 10:31:26 +0330

    adding room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,6 +42,12 @@ android {
 }
 
 dependencies {
+
+    // Room :
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
